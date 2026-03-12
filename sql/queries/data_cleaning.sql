49914dbe4b8066bffd1dcf54c8392e95da265293
Luca Crinò <crinoluca@gmail.com>
2026-03-12 23:19:41 +0100

    Update data_cleaning.sql

--- sql/queries/data_cleaning.sql
+++ sql/queries/data_cleaning.sql
@@ -1,1 +1,8 @@
+--Table: major_resource_projects
+--Objective: removing the comma from development_region attributes
+
+update table major_resource_project
+set development_region =
+left(development_region, position(',' in development_region) - 1) || right(development_region, length(development_region) - position(',' in development_region))
+
 
