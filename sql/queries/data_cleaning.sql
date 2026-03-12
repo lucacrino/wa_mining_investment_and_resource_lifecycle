d889a0da7b2953f5bc1c385ca07a2921e069aad0
Luca Crinò <crinoluca@gmail.com>
2026-03-12 23:21:41 +0100

    Fix comment and update development_region values

--- sql/queries/data_cleaning.sql
+++ sql/queries/data_cleaning.sql
@@ -1,8 +1,7 @@
 --Table: major_resource_projects
---Objective: removing the comma from development_region attributes
+--Objective: removing the comma from development_region values
 
-update table major_resource_project
-set development_region =
-left(development_region, position(',' in development_region) - 1) || right(development_region, length(development_region) - position(',' in development_region))
+update major_resource_project
+set development_region = left(development_region, position(',' in development_region) - 1) || right(development_region, length(development_region) - position(',' in development_region))
 
 
