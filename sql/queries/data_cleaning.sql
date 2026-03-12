c34f957f87f40b41841894acbdfe261f7b03f748
Luca Crinò <crinoluca@gmail.com>
2026-03-12 23:33:07 +0100

    Update data_cleaning.sql

--- sql/queries/data_cleaning.sql
+++ sql/queries/data_cleaning.sql
@@ -5,3 +5,11 @@ update major_resource_project
 set development_region = left(development_region, position(',' in development_region) - 1) || right(development_region, length(development_region) - position(',' in development_region))
 
 
+
+
+--Table: operating_mines
+--Objective: removing the comma from development_region values except for 'METRO'
+
+update operating_mines
+set development_region = left(development_region, position(',' in development_region) - 1) || right(development_region, length(development_region) - position(',' in development_region))
+where development_region <> 'METRO'
